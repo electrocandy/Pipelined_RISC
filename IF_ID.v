f2eb294872fe5ecc3736b77657c8cbbf0d5ec854
Anirudhan <anirudhans2004@gmail.com>
2024-10-22 18:22:12 +0530

    updated hazard controls

--- IF_ID.v
+++ IF_ID.v
@@ -1,5 +1,5 @@
-module IF_ID #(parameter N=32)(input clk,input pc_in,input flush,input [N-1:0]instruction_in,
-             output [N-1:0]instruction_out,output pc_out);
+module IF_ID #(parameter N=32)(input clk,input pc_in,input flush,input [N-1:0]instruction_in,input IFIDwrite,input [N-1:0]instruction_Hazard,
+               input pc_hazard,output [N-1:0]instruction_out,output pc_out);
 reg [N-1:0]instructionbuf;
 reg [N-1:0]pcbuf;             
 
@@ -8,7 +8,12 @@ begin
 if(flush) begin
    pcbuf<=32'b0;
    instructionbuf<=32'b0;
-end else begin
+end 
+else if(IFIDwrite) begin
+   pcbuf<=pc_hazard;
+   instructionbuf<=instruction_Hazard;
+end
+else begin
    pcbuf<=pc_in;
    instructionbuf<=instruction_in;
 end
@@ -16,4 +21,5 @@ end
 
 assign instruction_out=instructionbuf;
 assign pc_out=pcbuf;
+
 endmodule
