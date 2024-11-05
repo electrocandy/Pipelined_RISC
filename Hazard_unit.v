1825cb9a78c9c4296a0ac5e8a83e9ccf003f77d3
Anirudhan <anirudhans2004@gmail.com>
2024-11-05 18:40:43 +0530

    removed pc

--- Hazard_unit.v
+++ Hazard_unit.v
@@ -1,31 +1,33 @@
-module Hazard_unit #(parameter N=32)(input clk,input [2:0]IDEX_MEM,input [4:0]EXMEMregrd,input [N-1:0]instruction,input [N-1:0]pc_in,
-                                     output [N-1:0]pc,output [N-1:0]instruction_out, output IFIDwrite);
+module Hazard_unit #(parameter N=32)(input clk,input [2:0]IDEX_MEM,input [4:0]IDEXregrd,input [N-1:0]instruction,
+                                     output [N-1:0]instruction_out, output IFIDwrite,output Select);
+/*Check if pc is required in hazard unit, if required add it*/
 
 reg [4:0]r1;
 reg [4:0]r2;
-reg [N-1:0]pcbuf;
 reg [N-1:0]instructionbuf;
 reg ifidwritebuf;
+reg Selectbuf;
 
 initial begin
-   ifidwritebuf=1'b0; 
+   ifidwritebuf=1'b0;
+   Selectbuf=1'b1;
 end
 
 always@(posedge clk)begin
     r1=instruction[19:15]; /*R-type Instruction*/
     r2=instruction[24:20];
-    if(IDEX_MEM[0])begin
-        if(EXMEMregrd==r1 || EXMEMregrd==r2)begin
-          pcbuf<=pc_in;
+    if(IDEX_MEM[2])begin
+        if(IDEXregrd==r1 || IDEXregrd==r2)begin
           instructionbuf<=instruction;
           ifidwritebuf<=1'b1;
+          Selectbuf<=1'b0;
         end
     end
 end
 
 assign instruction_out=instructionbuf;
-assign pc_out=pc_in;
 assign IFIDwrite=ifidwritebuf;
+assign Select=Selectbuf;
 
 endmodule
           
