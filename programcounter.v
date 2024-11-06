71ba28413041fb4a4b9b06d0af60d07e273b2764
Anirudhan <anirudhans2004@gmail.com>
2024-11-07 00:28:07 +0530

    added hazard

--- programcounter.v
+++ programcounter.v
@@ -1,4 +1,4 @@
-module program_counter #(parameter N=32)(input branch, input zero,input [N-1:0]imm,input clk,output [N-1:0]pcout);
+module program_counter #(parameter N=32)(input clk,input [N-1:0]pc_in,input IFIDwrite,input [N-1:0]pc_hazard,output [N-1:0]pcout);
 
 reg [N-1:0]pcoutbuff;
 initial 
@@ -9,11 +9,10 @@ pcoutbuff=32'b0;
 end
 
 always@(posedge clk)begin
-if(branch==1'b1 && zero==1'b1) begin
-   pcoutbuff<=pcoutbuff+imm; 
-   end
-else begin
-   pcoutbuff<=pcoutbuff+1;
+   if(IFIDwrite)begin
+      pcoutbuff<=pc_hazard
+   end else begin
+      pcoutbuff<=pc_in;
    end
 end
 assign pcout=pcoutbuff;
