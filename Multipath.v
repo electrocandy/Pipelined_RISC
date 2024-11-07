3a0712cf6718d95a68fd182601ef1403cda14e7f
Anirudhan <anirudhans2004@gmail.com>
2024-11-07 22:11:14 +0530

    modified tb

--- Multipath.v
+++ Multipath.v
@@ -71,6 +71,16 @@ initial begin
     #50 clk=1'b1;
     #50 clk=1'b0;
     #50 clk=1'b1;
+    #50 clk=1'b0;
+    #50 clk=1'b1;
+    #50 clk=1'b0;
+    #50 clk=1'b1;
+    #50 clk=1'b0;
+    #50 clk=1'b1;
+    #50 clk=1'b0;
+    #50 clk=1'b1;
+    #50 clk=1'b0;
+    #50 clk=1'b1;
 end
 
 endmodule
