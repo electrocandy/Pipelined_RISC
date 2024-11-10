7d29d6b10d9d0dbd15e18445ac7bdfda18674ca0
Anirudhan <anirudhans2004@gmail.com>
2024-11-10 18:35:32 +0530

    added fixes

--- Multipath.v
+++ Multipath.v
@@ -4,7 +4,7 @@ wire IFIDwrite, flush, Select, equal;
 wire [N-1:0] instruction, pcout, imm, instruction_out, pc_out, pc, 
              data_frommux, data1, data2, imm_out, Alu_data, dt1, dt2, 
              muxAout, muxBout, Writedata, mem_in, mem_out, data_out, Aluresout, 
-             muxDin1, muxDin2, muxDout,instruc_out;
+             /*muxDin1, muxDin2, muxDout,*/instruc_out;
              
 wire [2:0] MEM_out, MEM_in, EX_in, EX_out, MEM_out_EXMEM;
 wire [1:0] WB_in, WB_out, WB_out_EXMEM, WB_out_MEMWB, ForwardA, ForwardB;
@@ -16,15 +16,15 @@ wire [7:0] con,zero;
 assign zero=8'b0;
 assign flush=1'b0;
 
-mux21 insD(muxDin1,muxDin2,equal,muxDout);
-program_counter ins1(clk,muxDout,IFIDwrite,pc,pcout);
-nbitadder ins16(pcout,1,muxDin1);
+//mux21 insD(muxDin1,muxDin2,equal,muxDout);
+program_counter ins1(clk,pc_out,imm,IFIDwrite,equal,pc,pcout);
+//nbitadder ins16(pcout,1,muxDin1);
 instruction_memory ins2(pcout,clk,instruction);
 IF_ID ins3(clk,pcout,flush,instruction,IFIDwrite,/*instruction_Hazard,pc_hazard*/instruction_out,pc_out);
 Hazard_unit ins4(clk,MEM_out,Loadregout,instruction_out,pc_out,pc,IFIDwrite,Select);
-register ins5(instruction_out[19:15],instruction_out[24:20],data_frommux,clk,instruction_out[11:7],WB_out_MEMWB[0],data1,data2);
+register ins5(instruction_out[19:15],instruction_out[24:20],data_frommux,clk,Loadregout_out_out,WB_out_MEMWB[0],data1,data2);
 immediategenerate ins6(instruction_out,imm);
-nbitadder ins17(pc_out,imm,muxDin2);
+//nbitadder ins17(pc_out,imm,muxDin2);
 control ins7(instruction_out[6:0],clk,MEM_in[0],MEM_in[2],WB_in[1],EX_in[2:1],MEM_in[1],EX_in[0],WB_in[0]);
 assign con={EX_in,MEM_in,WB_in};
 Branch_Predictor ins18(data1,data2,MEM_in[0],equal);
@@ -36,7 +36,7 @@ mux31 insA(dt1,data_frommux,mem_in/*From EX_MEM pipe*/,ForwardA,muxAout);
 mux31 insB(dt2,data_frommux,mem_in/*From EX_MEM pipe*/,ForwardB,muxBout);
 ALU ins10(muxAout,muxBout,Alucon,Alu_data);
 Data_Forwarding ins11(Loadregout_out,Loadregout_out_out,WB_out_EXMEM,WB_out_MEMWB,outreg1,outreg2,ForwardA,ForwardB);
-EX_MEM ins12(clk,MEM_in,WB_in,Alu_data,Loadregout,muxBout,MEM_out_EXMEM,WB_out_EXMEM,Writedata,mem_in,Loadregout_out);
+EX_MEM ins12(clk,MEM_out,WB_out,Alu_data,Loadregout,muxBout,MEM_out_EXMEM,WB_out_EXMEM,Writedata,mem_in,Loadregout_out);
 data_memory ins13(mem_in,Writedata,clk,MEM_out_EXMEM[2],MEM_out_EXMEM[1],mem_out);
 MEM_WB ins14(clk,WB_out_EXMEM,Loadregout_out,mem_out,mem_in,data_out,Aluresout,Loadregout_out_out,WB_out_MEMWB);
 mux21 ins15(data_out,Aluresout,WB_out_MEMWB[1],data_frommux);
--- programcounter.v
+++ programcounter.v
@@ -1,20 +1,27 @@
-module program_counter #(parameter N=32)(input clk,input [N-1:0]pc_in,input IFIDwrite,input [N-1:0]pc_hazard,output [N-1:0]pcout);
+module program_counter #(parameter N=32)(input clk,input [N-1:0]pc_out,input [N-1:0]imm,
+                                         input IFIDwrite,input equal,input [N-1:0]pc_hazard,output [N-1:0]pcout);
 
 reg [N-1:0]pcoutbuff;
+reg [N-1:0]pcoutbuff1;
+reg [N-1:0]pcoutbuff2;
 initial 
 $display("Simulation started");
 
 initial begin
 pcoutbuff=32'b0;
+pcoutbuff1=32'b0;
+pcoutbuff2=32'b0;
 end
 
 always@(posedge clk)begin
    if(IFIDwrite)begin
       pcoutbuff<=pc_hazard;
+   end else if(equal) begin
+      pcoutbuff1<=pc_out+imm;
    end else begin
-      pcoutbuff<=pc_in;
+      pcoutbuff2<=pcoutbuff2+1;
    end
 end
-assign pcout=pcoutbuff;
+assign pcout=(IFIDwrite) ? pcoutbuff : (equal) ? pcoutbuff1 : pcoutbuff2;
 
 endmodule
